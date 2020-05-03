026b5c19ea7925f3929bde095140f9dd3376e1de
Rodrigo <rodeagle@live.com>
2020-05-02 21:04:03 -0500

    * Added new app module handler to instantiate a app on a view with customer app for each page instead of a gobal one, but has some global config * Boostrapper added new modules *Index remove the app from there, code cleanup * Add expanded, the component for images now work fixing the controller * header layout code cleanup

--- ListaNegra/Content/scripts/RequireJS/bootstrapper.js
+++ ListaNegra/Content/scripts/RequireJS/bootstrapper.js
@@ -1,5 +1,5 @@
 ﻿requirejs.config({
-    baseUrl: 'Scripts',
+    baseUrl: '../Content/scripts',
     paths: {
         'jquery': 'https://code.jquery.com/jquery-3.4.1.min',
         'bootstrap': 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min',
@@ -7,7 +7,8 @@
         'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue',
         'vuetify' : 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify',
         'tag-input-vue': 'https://unpkg.com/@johmun/vue-tags-input/dist/vue-tags-input',
-        'input-mask': 'https://cdn.jsdelivr.net/npm/vue-the-mask@0.11.1/dist/vue-the-mask.min'
+        'input-mask': 'https://cdn.jsdelivr.net/npm/vue-the-mask@0.11.1/dist/vue-the-mask.min',
+        'app' : 'main/app'
     },
     shim: {
         'vuetify': {
@@ -18,9 +19,11 @@
         },
         'input-mask': {
             deps: ['vue']
+        },
+        'app': {
+            deps: ['vue', 'vuetify', 'input-mask']
         }
     }
 });
 
-require(['jquery', 'bootstrap']);
 
---
+++ ListaNegra/Content/scripts/main/app.js
@@ -0,0 +1,48 @@
+﻿define(['vue', 'vuetify', 'input-mask','jquery'], function (Vue, Vuetify, VueMask, $) {
+    //add the vue plugin on start up
+    Vue.use(Vuetify);
+    Vue.use(VueMask);
+
+    Vue.component('input-image-hidden', {
+        props: {
+            url: Array
+        },
+        template: `<div>
+                        <small>Imagenes agregadas</small>
+                        <div v-for="item in url" class="input-group">
+                            <input type="textbox" class="form-control" :value="item"/><v-icon class="append-addon clickable">add</v-icon>
+                        </div>
+                    </div>`,
+        methods: {
+            remove: function () {
+                delete this.url;
+            }
+        }
+    });
+
+    var app = {
+        vuetify: new Vuetify()
+        //components: {
+        //    'manage-add-controller': 'manage-add-controller'
+        //}
+
+    };
+
+    return {
+        Start: function (settings) {
+            app = $.extend({}, app, settings);
+            if (app.data) {
+                app.data['isMounted'] = false;
+            } else {
+                app.data = { isMounted : false };
+            }
+            let $app = new Vue(app).$mount('#MainApp');
+            console.log(1);
+            $app.$data.isMounted = true;
+            window.addEventListener('hashchange', function () {
+                alert('test');
+                $app.$data.isMounted = false;
+            }, false);
+        }
+    };
+});
